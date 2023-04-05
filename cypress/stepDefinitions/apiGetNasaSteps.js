998f0301b12a4bb7bd7a32109c221a6d318c0c00
alexisrsalazar <121194414+alexisrsalazar@users.noreply.github.com>
2023-04-05 12:28:32 -0600

    fixes

--- cypress/stepDefinitions/apiGetNasaSteps.js
+++ cypress/stepDefinitions/apiGetNasaSteps.js
@@ -9,7 +9,7 @@ Given("I am in Nasa API", () => {
     baseUrl = "https://api.nasa.gov/"
 })
 
-When("I want to get the APOD and I call the API with '<date>' parameter", (date) => {
+When("I want to get the APOD and I call the API with {string} parameter", (date) => {
     apiUrl = baseUrl + "planetary/apod?date=" + date + '&api_key=' + apiKey.api_Key
     request = cy.request({
         method: 'GET',
