dbe000dfe62706d27b190b3613a8545e635bdec4
alexisrsalazar <121194414+alexisrsalazar@users.noreply.github.com>
2023-04-05 12:22:05 -0600

    Some fixes in the code and add a new script

--- cypress/e2e/features/apiGetNasa.feature
+++ cypress/e2e/features/apiGetNasa.feature
@@ -1,7 +1,7 @@
 Feature: API Call GET in NASA API
 
     Scenario: I want get the Astronomy Picture of the Day
-        Given I want to GET the Astronomy Picture of the Day
+        Given I am in Nasa API
         When I call the API with '<date>' parameter
         Then I should receive 200 response code and response body '<title>'
 
@@ -9,3 +9,8 @@ Feature: API Call GET in NASA API
             | date       | title                               |
             | 1996-07-12 | Ancient Cratered Plains on Ganymede |
             | 2021-06-18 | Devil Horns from a Ring of Fire     |
+
+    Scenario: I want to get Mars Weather Service API
+        Given I am in Nasa API
+        When I want to GET InSight: Mars Weather Service API
+        Then I should receive 200 response code and sol hours required
--- cypress/stepDefinitions/apiGetNasaSteps.js
+++ cypress/stepDefinitions/apiGetNasaSteps.js
@@ -5,12 +5,20 @@ let apiUrl;
 let baseUrl;
 let request;
 
-Given("I want to GET the Astronomy Picture of the Day", () => {
-    baseUrl = "https://api.nasa.gov/planetary/apod"
+Given("I am in Nasa API", () => {
+    baseUrl = "https://api.nasa.gov/"
 })
 
 When("I call the API with {string} parameter", (date) => {
-    apiUrl = baseUrl + "?date=" + date + '&api_key=' + apiKey.api_Key
+    apiUrl = baseUrl + "planetary/apod" + "?date=" + date + '&api_key=' + apiKey.api_Key
+    request = cy.request({
+        method: 'GET',
+        url: apiUrl
+    })
+})
+
+When("I want to GET InSight: Mars Weather Service API", () => {
+    apiUrl = baseUrl + "insight_weather/?api_key=" + apiKey.api_Key + "&feedtype=json&ver=1.0"
     request = cy.request({
         method: 'GET',
         url: apiUrl
@@ -23,3 +31,12 @@ Then("I should receive 200 response code and response body {string}", (title) =>
         expect(res.body.title).to.eq(title)
     })
 })
+
+Then("I should receive 200 response code and sol hours required", () => {
+    request.then((res)=> {
+        expect(res.status).to.eq(200)
+        expect(res.body.validity_checks.sol_hours_required).to.eq(18)
+    })
+})
+
+
